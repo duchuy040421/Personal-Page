bad382755daab77edff072261e851446fc4c9edd
Nguyen Duc Huy <57801256+duchuy040421@users.noreply.github.com>
2022-02-14 11:45:06 +0700

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,17 +1,18 @@
 import knexObj from "knex";
 
 export const connectionInfo = {
-    host : '127.0.0.1',
-    port : 3333,
-    user : 'root',
-    password : '',
+    host : 'connectionInfo',
+    port : 5432,
+    user : 'xgyvuqhswxxkgw',
+    password : 'bb014ff3c7c14799605821f1a0d300748d33574dc3b8008cd51d40fb7f992d9b',
     database : 'personalweb'
 }
 
 const knex = knexObj({
-    client: 'mysql2',
-    connection: connectionInfo,
-    pool: { min: 0, max: 20 }
+  client: 'pg',
+  version: '7.2',
+  connection: connectionInfo,
+  pool: { min: 0, max: 20 }
 });
 
 export default knex;
