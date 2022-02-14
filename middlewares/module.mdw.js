05a8015ecce0ce59c3e62892390f286b1051b355
Nguyen Duc Huy <duchuy040421@gmail.com>
2022-02-14 11:08:19 +0700

    90%

--- middlewares/module.mdw.js
+++ middlewares/module.mdw.js
@@ -12,7 +12,7 @@ export default function (app){
     routesMdw(app);
     errorMdw(app);
 
-    const port = 3000;
+    const port = process.env.PORT || 3000;
     app.listen(port, function(){
         console.log(`Personal app listening on port ${port}`)
     })
